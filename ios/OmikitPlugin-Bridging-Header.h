a275e9082b84d948ea16bd8b0db79b5963914844
Steve <chaunguyen4297@gmail.com>
2023-04-20 11:17:33 +0700

    feat(ios): process click notification for iOS <chaunn>

--- ios/OmikitPlugin-Bridging-Header.h
+++ ios/OmikitPlugin-Bridging-Header.h
@@ -1,2 +1,12 @@
-#import <React/RCTBridgeModule.h>
-#import <React/RCTViewManager.h>
+//
+//  OmikitPlugin-Bridging-Header.h
+//  Pods
+//
+//  Created by PRO 2019 16' on 20/04/2023.
+//
+
+#ifndef OmikitPlugin_Bridging_Header_h
+#define OmikitPlugin_Bridging_Header_h
+
+
+#endif /* OmikitPlugin_Bridging_Header_h */
