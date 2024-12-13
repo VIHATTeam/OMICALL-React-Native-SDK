0fc53e6b47c7f0b2c8e05f8bbbf342a20717c1f3
HungTH <tranhoaihung01@gmail.com>
2024-12-13 14:52:14 +0700

    feat: add config typescript

---
+++ src/types/omikit-plugin.d.ts
@@ -0,0 +1,43 @@
+declare module 'omikit-plugin' {
+    import { NativeEventEmitter } from 'react-native';
+  
+    export function startServices(): Promise<any>;
+    export function configPushNotification(data: any): Promise<any>;
+    export function getInitialCall(): Promise<any>;
+    export function initCallWithUserPassword(data: any): Promise<boolean>;
+    export function initCallWithApiKey(data: any): Promise<boolean>;
+    export function startCall(data: any): Promise<boolean>;
+    export function startCallWithUuid(data: any): Promise<boolean>;
+    export function joinCall(): Promise<any>;
+    export function endCall(): Promise<any>;
+    export function toggleMute(): Promise<boolean>;
+    export function toggleSpeaker(): Promise<boolean>;
+    export function onHold(data: any): Promise<boolean>;
+    export function sendDTMF(data: any): Promise<boolean>;
+    export function switchOmiCamera(): Promise<boolean>;
+    export function toggleOmiVideo(): Promise<boolean>;
+    export function logout(): Promise<boolean>;
+    export function registerVideoEvent(): Promise<boolean>;
+    export function removeVideoEvent(): Promise<boolean>;
+    export function getCurrentUser(): Promise<any>;
+    export function getGuestUser(): Promise<any>;
+    export function systemAlertWindow(): Promise<boolean>;
+    export function openSystemAlertSetting(): Promise<void>;
+    export function getAudio(): Promise<any>;
+    export function setAudio(data: any): Promise<void>;
+    export function getCurrentAudio(): Promise<any>;
+    export function transferCall(data: any): Promise<boolean>;
+  
+    export const omiEmitter: NativeEventEmitter;
+  
+    export const OmiCallEvent: {
+      onCallStateChanged: string;
+      onSpeaker: string;
+      onMuted: string;
+      onRemoteVideoReady: string;
+      onClickMissedCall: string;
+      onSwitchboardAnswer: string;
+      onCallQuality: string;
+      onAudioChange: string;
+    };
+  }
