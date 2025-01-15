4440ee490e0b6ce7384c1f8f50bb98c674eddeda
HungTH <tranhoaihung01@gmail.com>
2025-01-15 17:05:44 +0700

    fix: change import file objective C

--- ios/Library/OmikitNotification.m
+++ ios/Library/OmikitNotification.m
@@ -6,8 +6,13 @@
 //
 
 #import <Foundation/Foundation.h>
+#if __has_include("OmikitNotification.h")
+#import "OmikitNotification.h"
+#elif __has_include(<OmikitPlugin/OmikitPlugin-Swift.h>)
 #import <OmikitPlugin/OmikitPlugin-Swift.h>
-
+#else
+#import <omikit_plugin/OmikitNotification.h>
+#endif
 
 @implementation OmikitNotification : NSObject 
 + (void)didRecieve:(NSDictionary*) userInfo{
